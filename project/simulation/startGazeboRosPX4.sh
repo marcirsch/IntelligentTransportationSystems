9ec1678a9b0989bc2179b9098a207f9f5988cf54
Marcell Rausch <marcell.rausch@gmail.com>
2019-11-11 17:20:03 +0100

    Some fixes

--- project/simulation/startGazeboRosPX4.sh
+++ project/simulation/startGazeboRosPX4.sh
@@ -1,10 +1,10 @@
 #source ros location
-source /opt/ros/melodic/setup.bash
+source /opt/ros/melodic/setup.bash 
 
 #Run Firmware with gazebo_ros
-source ../Firmware/Tools/setup_gazebo.bash $(pwd) ..//Firmware/build/px4_sitl_default
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:../Firmware
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:../Firmware/Tools/sitl_gazebo
+source ../Firmware/Tools/setup_gazebo.bash ~/IntelligentTransportationSystems/project/Firmware ~/IntelligentTransportationSystems/project/Firmware/build/px4_sitl_default
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/IntelligentTransportationSystems/project/Firmware
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/IntelligentTransportationSystems/project/Firmware/Tools/sitl_gazebo
 
 #Start simulation
 roslaunch px4 posix_sitl.launch
